ba5dc024abccb4b1616c0d8bb29e82fc67c9371c
Tobias Mädel <t.maedel@alfeld.de>
2021-02-14 02:13:30 +0100

    librespot: Add buildroot package

---
+++ buildroot/package/librespot/librespot.mk
@@ -0,0 +1,62 @@
+################################################################################
+#
+# librespot
+#
+################################################################################
+
+LIBRESPOT_VERSION = master
+LIBRESPOT_SITE = git@github.com:librespot-org/librespot.git
+LIBRESPOT_SITE_METHOD = git
+LIBRESPOT_LICENSE = MIT
+LIBRESPOT_LICENSE_FILES = LICENSE
+LIBRESPOT_INSTALL_TARGET = YES
+LIBRESPOT_INSTALL_STAGING = YES
+
+LIBRESPOT_DEPENDENCIES = host-rustc
+
+LIBRESPOT_CARGO_ENV = \
+    CARGO_HOME=$(HOST_DIR)/usr/share/cargo \
+    RUST_TARGET_PATH=$(HOST_DIR)/etc/rustc \
+    TARGET_CC=$(TARGET_CC) \
+    CC=$(TARGET_CC) \
+    PKG_CONFIG_ALLOW_CROSS=1
+
+LIBRESPOT_CARGO_OPTS = \
+    --target=${RUSTC_TARGET_NAME} \
+    --manifest-path=$(@D)/Cargo.toml \
+    --no-default-features
+
+ifeq ($(BR2_ENABLE_DEBUG),y)
+LIBRESPOT_CARGO_MODE = debug
+else
+LIBRESPOT_CARGO_MODE = release
+endif
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
+LIBRESPOT_CARGO_FEATURES += alsa-backend
+endif
+
+ifeq ($(BR2_PACKAGE_PORTAUDIO),y)
+LIBRESPOT_CARGO_FEATURES += portaudio-backend
+endif
+
+ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
+LIBRESPOT_CARGO_FEATURES += pulseaudio-backend
+endif
+
+LIBRESPOT_CARGO_OPTS += --${LIBRESPOT_CARGO_MODE} --features "${LIBRESPOT_CARGO_FEATURES}"
+
+define LIBRESPOT_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(LIBRESPOT_CARGO_ENV) \
+            cargo build $(LIBRESPOT_CARGO_OPTS)
+endef
+
+define LIBRESPOT_INSTALL_TARGET_CMDS
+    $(INSTALL) -D \
+            $(@D)/target/$(RUSTC_TARGET_NAME)/$(LIBRESPOT_CARGO_MODE)/librespot \
+            $(TARGET_DIR)/usr/bin/librespot
+            
+    $(TARGET_STRIP) --strip-all $(TARGET_DIR)/usr/bin/librespot
+endef
+
+$(eval $(generic-package))
