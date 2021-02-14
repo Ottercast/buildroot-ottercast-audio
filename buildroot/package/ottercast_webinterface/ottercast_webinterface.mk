ad8c52c743a2184738302891e195543a621a2612
Tobias Schramm <t.schramm@t-sys.eu>
2021-02-14 17:53:44 +0100

    ottercast_webinterface: Fix makefile
    Make safe for rebuilds, replace cp ny install where possible

--- buildroot/package/ottercast_webinterface/ottercast_webinterface.mk
+++ buildroot/package/ottercast_webinterface/ottercast_webinterface.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OTTERCAST_WEBINTERFACE_VERSION = 0.1
+OTTERCAST_WEBINTERFACE_VERSION = 0.1.1
 OTTERCAST_WEBINTERFACE_SITE = $(call github,Manawyrm,ottercast-audio-webif,v$(OTTERCAST_WEBINTERFACE_VERSION))
 OTTERCAST_WEBINTERFACE_LICENSE = MIT
 OTTERCAST_WEBINTERFACE_LICENSE_FILES = LICENSE
@@ -15,12 +15,13 @@ define OTTERCAST_WEBINTERFACE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/var/www/
 	cp -r $(@D)/* $(TARGET_DIR)/var/www/
 
-	cp -r $(OTTERCAST_WEBINTERFACE_PKGDIR)/ottercast-config.service $(TARGET_DIR)/usr/lib/systemd/system/
-	ln -s usr/lib/systemd/system/ottercast-config.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/ottercast-config.service
+	$(INSTALL) -D -m 0644 $(OTTERCAST_WEBINTERFACE_PKGDIR)/ottercast-config.service $(TARGET_DIR)/usr/lib/systemd/system/
+	ln -fs usr/lib/systemd/system/ottercast-config.service $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/ottercast-config.service
 
-	mkdir -p $(TARGET_DIR)/etc/default/
-	cp $(OTTERCAST_WEBINTERFACE_PKGDIR)/librespot $(TARGET_DIR)/etc/default/librespot 
-	cp $(OTTERCAST_WEBINTERFACE_PKGDIR)/uhttpd $(TARGET_DIR)/etc/default/uhttpd
+	$(INSTALL) -D -m 0644 $(OTTERCAST_WEBINTERFACE_PKGDIR)/librespot $(TARGET_DIR)/etc/default/librespot
+	$(INSTALL) -D -m 0644 $(OTTERCAST_WEBINTERFACE_PKGDIR)/uhttpd $(TARGET_DIR)/etc/default/uhttpd
+
+	$(INSTALL) -D -m 0644 $(@D)/example_config.sh  $(BINARIES_DIR)/config.sh
 endef
 
 $(eval $(generic-package))
