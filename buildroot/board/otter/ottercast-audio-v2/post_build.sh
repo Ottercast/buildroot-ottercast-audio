ef75aca830662634271486f360cc22aafa5d5cd8
Tobias Schramm <t.schramm@t-sys.eu>
2021-02-14 16:36:14 +0100

    board: ottercast-audio-v2: include ottercast config in fat boot partition

--- buildroot/board/otter/ottercast-audio-v2/post_build.sh
+++ buildroot/board/otter/ottercast-audio-v2/post_build.sh
@@ -5,3 +5,10 @@ set -e
 BOARD_DIR="$(dirname $0)"
 
 cp -r "$BOARD_DIR"/skeleton/* "$TARGET_DIR"
+
+
+OTTERCAST_CONFFILE="$BINARIES_DIR"/config.sh
+
+if ! [ -f "$OTTERCAST_CONFFILE" ]; then
+	touch "$OTTERCAST_CONFFILE"
+fi
