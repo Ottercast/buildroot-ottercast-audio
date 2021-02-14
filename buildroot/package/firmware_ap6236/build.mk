24d98661926c2556928832250cce30a8f8f3e650
Tobias Schramm <t.schramm@t-sys.eu>
2021-02-14 03:35:18 +0100

    Add bluetooth support

--- buildroot/package/firmware_ap6236/build.mk
+++ buildroot/package/firmware_ap6236/build.mk
@@ -7,6 +7,7 @@ FIRMWARE_AP6236_SITE =
 define FIRMWARE_AP6236_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 644 $(FIRMWARE_AP6236_PKGDIR)/firmware/nvram_ap6236.txt $(TARGET_DIR)/usr/lib/firmware/brcm/brcmfmac43430-sdio.txt
 	$(INSTALL) -D -m 644 $(FIRMWARE_AP6236_PKGDIR)/firmware/fw_bcm43436b0.bin $(TARGET_DIR)/usr/lib/firmware/brcm/brcmfmac43430-sdio.bin
+	$(INSTALL) -D -m 644 $(FIRMWARE_AP6236_PKGDIR)/firmware/BCM4343B0.hcd $(TARGET_DIR)/usr/lib/firmware/brcm/BCM.hcd
 endef
 
 $(eval $(generic-package))
