201abdb9f311db9b93a66881ca526e8190e2b504
Tobias Schramm <tobias@t-sys.eu>
2023-01-30 23:44:58 +0100

    package: firmware_ap6255: bump firmware repo to v1.0.1
    Changelog:
     - update firmware for 802.11w support on AP6255

--- buildroot/package/firmware_ap6255/build.mk
+++ buildroot/package/firmware_ap6255/build.mk
@@ -1,13 +1,17 @@
 
-FIRMWARE_AP6255_VERSION = 1.0.0
+FIRMWARE_AP6255_VERSION = 1.0.1
 FIRMWARE_AP6255_LICENSE = Redistributable
 FIRMWARE_AP6255_LICENSE_FILES = LICENSE
 FIRMWARE_AP6255_SITE = $(call github,OtterCast,broadcom-firmware,v$(FIRMWARE_AP6255_VERSION))
 
 define FIRMWARE_AP6255_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 644 $(@D)/brcmfmac43455-sdio.bin \
+	$(INSTALL) -D -m 644 $(@D)/cyfmac43455-sdio.bin \
+		$(TARGET_DIR)/usr/lib/firmware/cypress/cyfmac43455-sdio.bin
+	ln -f -s ../cypress/cyfmac43455-sdio.bin \
 		$(TARGET_DIR)/usr/lib/firmware/brcm/brcmfmac43455-sdio.bin
-	$(INSTALL) -D -m 644 $(@D)/brcmfmac43455-sdio.clm_blob \
+	$(INSTALL) -D -m 644 $(@D)/cyfmac43455-sdio.clm_blob \
+		$(TARGET_DIR)/usr/lib/firmware/cypress/cyfmac43455-sdio.clm_blob
+	ln -f -s ../cypress/cyfmac43455-sdio.clm_blob \
 		$(TARGET_DIR)/usr/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob
 	$(INSTALL) -D -m 644 $(@D)/brcmfmac43455-sdio.txt \
 		$(TARGET_DIR)/usr/lib/firmware/brcm/brcmfmac43455-sdio.txt
